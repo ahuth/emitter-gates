ff9b40f15daa497f6283022720909ac37b31c906
Andrew Huth <andrew@huth.me>
2020-05-26 06:41:24 -0700

    Add Or

---
+++ src/Or.ts
@@ -0,0 +1,39 @@
+import Wire from './Wire';
+
+export default class Or {
+  output = new Wire();
+  value1 = false;
+  value2 = false;
+
+  constructor(input1: Wire, input2: Wire) {
+    input1.subscribe((value) => {
+      this.value1 = value === 1;
+      this.check();
+    });
+
+    input2.subscribe((value) => {
+      this.value2 = value === 1;
+      this.check();
+    });
+  }
+
+  check() {
+    if (this.value1) {
+      this.output.allowCurrent();
+      return;
+    }
+
+    if (this.value2) {
+      this.output.allowCurrent();
+      return;
+    }
+
+    this.output.stopCurrent();
+  }
+
+  subscribe(callback: (value: 0 | 1) => void) {
+    this.output.subscribe(function (value) {
+      callback(value);
+    });
+  }
+}
